872672a5fec6d1f3c06f18afc906baf0060f4301
Gerard KESSE <tiakagerard@hotmail.com>
2017-07-14 00:33:43 +0200

    Initial Commit

---
+++ doc_Markdown.sh
@@ -0,0 +1,74 @@
+#================================================
+# Markdown 
+#================================================
+
+# Markdown
+-> langage de balisage léger créé par John Gruber en 2004. 
+-> offrir une syntaxe facile à lire et à écrire.
+
+# mettre du texte en emphase (balise HTML <em>)
+*quelques mots* ou  
+_quelques mots_
+
+# mettre du texte en grande emphase (balise HTML <strong>)
+**plus important** ou 
+__également important__
+
+# mettre du code dans le texte (balise HTML <code>)
+Mon texte `code` fin de mon texte
+
+# paragraphe de code, mettre quatre espaces devant
+    Première ligne de code
+    Deuxième ligne
+
+# faire des citations
+> Ce texte apparaîtra dans un élément HTML <blockquote>.
+
+# faire un nouveau paragraphe, sauter une ligne 
+Premier paragraphe
+
+Deuxième paragraphe   
+
+# faire un simple retour à la ligne
+-> mettre deux espaces en fin de ligne (balise HTML <br>).
+
+# créer une liste non ordonnée (balise HTML <ul>)
+* Pommes
+* Poires
+    * Sous élément avec au moins quatre espaces devant.
+
+# liste ordonnée (balise HTML <ol>) :
+1. mon premier
+2. mon deuxième
+
+# liste en mode case à cocher
+- [ ] Case non cochée
+- [x] Case cochée
+
+# titres
+-> créés avec un certain nombre de \# avant le titre
+----------
+\# un titre de premier niveau
+\#### un titre de quatrième niveau
+
+# les deux premiers niveaux de titre (<h1> et <h2>)
+-> souligner le titre avec des = ou des -
+----------
+Titre de niveau 1
+=====================
+Titre de niveau 2
+-------------------
+
+# créer des tableaux (balises HTML <tr> et <th>)
+| Titre 1       |     Titre 2     |   Titre 3      |
+|---------------|-----------------|----------------|
+| Colonne       |     Colonne     |      Colonne   |
+| Alignée à     |   Alignée au    |     Alignée à  |
+| Gauche        |     Centre      |      Droite    |
+
+# créer des liens (balise HTML <a>)
+[texte du lien](url_du_lien "texte pour le titre, facultatif")
+
+# Les images s'insèrent de la même façon que les liens
+![Zozor](http://uploads.siteduzero.com/files/420001_421000/420263.png)
+
--- doc_Qt.sh
+++ doc_Qt.sh
@@ -1,9 +1,59 @@
 #================================================
-# Qt
+# Qt Creator
 #================================================
+
+#------------------------------------------------
+# TextEditor
 
 # Faire un zoom avant
 CTRL + [+]
+----------
+-> Augmenter la taille des caractère
 
 # Faire un zoom arrière
 CTRL + [-]
+----------
+-> Réduire la taille des caractère
+
+# Identer les lignes de code
+CTRL + A -> Sélectionner
+CTRL + I -> Identer
+----------
+-> Réorganiser les lignes de code
+
+#------------------------------------------------
+# Widget
+
+# Promouvoir un widget
+QWidget -> Right Click
+Promouvoir en
+Nom de la classe promue -> GMenu
+Fichier d en-tête -> GMenu.h
+Ajouter
+Promouvoir
+----------
+-> Passer de QWidget à GMenu
+
+# Rétrograder un widget
+QWidget -> Right Click
+Rétrograder en QWidget
+----------
+-> Passer de GMenu à QWidget
+
+# Etirer les lignes de disposition
+Layout
+LayoutRowStretch -> 0,1,0
+----------
+-> Etirer la ligne 2 parmi les 3 lignes
+
+# Etirer les colonnes de disposition
+Layout
+LayoutColumnStretch -> 0,1,0
+----------
+-> Etirer la colonne 2 parmi les 3 colonnes
+
+
+
+
+
+
