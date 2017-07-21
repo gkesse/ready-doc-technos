b80f3f7b4fac1ec2d8d4810e9e528828ea6b8de9
Gerard KESSE <tiakagerard@hotmail.com>
2017-07-21 06:05:33 +0200

    Initial Commit

---
+++ doc_Qt_ModelView.sh
@@ -0,0 +1,18 @@
+#================================================
+# Qt - ModelView
+#================================================
+
+# QTableView::setMode
+Définit le modèle de la vue à présenter.
+
+# Qt::DisplayRole
+Les données clés à fournir sous forme de texte. (QString)
+
+
+
+
+
+
+
+
+
