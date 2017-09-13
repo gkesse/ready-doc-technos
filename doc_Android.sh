2cb9f40f13d5467f03bf4137674c87b1100af5a6
Gerard KESSE <tiakagerard@hotmail.com>
2017-09-13 07:57:57 +0200

    Initial Commit

--- doc_Android.sh
+++ doc_Android.sh
@@ -3,23 +3,58 @@
 #================================================
 
 #------------------------------------------------
+# Configurer Android NDK
+#------------------------------------------------
+# Installer Java
+-> Installer Java
+
+# Configurer la variable d environnement JAVA_HOME
+JAVA_HOME -> JAVA_ROOT_DIR
+
+# Vérifier l installation de Java
+javac -version
+
+# Installer Cygwin
+-> Installer make
+-> Installer gawk
+
+# Installer Eclipse
+-> Installer Eclipse
+
+# Installer Android ADT
+Help -> Install New Software -> Add
+Name -> ADT
+Location -> https://dl-ssl.google.com/android/eclipse/ -> Ok
+Check -> Developer Tools -> Next -> Next
+Check -> I accept the terms of the licence agreements -> Finish
+
+# Installer Android SDK
+ -> Installer Android SDK
+
+# Configurer Android SDK
+Window -> Preferences -> Android
+SDK Location -> C:\NVPACK\android-sdk-windows -> Ok
+
+#------------------------------------------------
+# Android ADT
+#------------------------------------------------
+-> Android Development Tools
+-> Outils de Développement Android
+-> Permet de développer sous Android
+
+#------------------------------------------------
+# Android SDK
+#------------------------------------------------
+-> Software Development Kit
+-> Kit de Développement d Application Android
+-> Permet de développer des applications Android
+
+#------------------------------------------------
 # Android NDK
 #------------------------------------------------
-# NDK
 -> Native Development Kit
--> Kit de Développement Native
-
-# But
--> Permet de développer des applications Android 
--> dans un langage natif tel que le C/C++.
-
-# Avantages
--> Améliorer les performances d une application Android
--> Qui demande de la puissance processeur
--> Telle que les applications multimédia et de jeux vidéo
-
-
-
+-> Kit de Développement Natif
+-> Permet de développer en C/C++ sous Android
 
 
 
