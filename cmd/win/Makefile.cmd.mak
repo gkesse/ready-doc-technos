3cf7de66d5af5b642a52413f7ae1cc59fa0f65f4
Gerard KESSE <tiakagerard@hotmail.com>
2021-02-25 12:41:06 +0100

    Initial Commit

---
+++ cmd/win/Makefile.cmd.mak
@@ -0,0 +1,9 @@
+#================================================
+all:   
+#================================================    
+# git
+git_push:
+	@cd $(GPROJECT_PATH) && git pull && git add --all && git commit -m "Initial Commit" && git push -u origin master
+git_clone:
+	@cd $(GPROJECT_ROOT) && git clone $(GGIT_URL) $(GGIT_NAME) 
+#================================================    
---
+++ old/ReadyProject.sh
@@ -0,0 +1,13 @@
+#================================================
+# Rapports
+#================================================
+GProject_smart_city(){}
+#================================================
+thématiques traitées; 
+1 smart amenagement et circulation, 
+2 smart énergie et grid. 
+3 smart data edge pour réseau de capteurs de mesures des données de la ville. 
+4 deep learning et data analyse
+#================================================
+GProject_notes_et_references(){}
+#================================================
